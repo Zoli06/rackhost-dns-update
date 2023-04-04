3e8fef15e440fdf13d7542b0eb7cedaec4dd0e2e
Zoli06 <zolixvagyok@gmail.com>
2023-04-04 09:29:29 +0200

    Listen for get requests

--- ddns.py
+++ ddns.py
@@ -3,31 +3,21 @@
 
 import http.server
 
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Log url parameters
+        print(self.path)
+        print(self.headers)
+        print(self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8'))
+        
+        self.send_response(200)        
+        
+        return
+
 def main(server_class=http.server.HTTPServer, handler_class=http.server.BaseHTTPRequestHandler):
     server_address = ('', 8245)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
-    
-    # Log the request
-    print('Request received')
-    print('Request headers:')
-    print(httpd.headers)
-    print('Request body:')
-    print(httpd.body)
-    print('Request method:')
-    print(httpd.method)
-    print('Request path:')
-    print(httpd.path)
-    print('Request version:')
-    print(httpd.version)
-    print('Request command:')
-    print(httpd.command)
-    print('Request requestline:')
-    print(httpd.requestline)
-    print('Request client_address:')
-    print(httpd.client_address)
-    print('----------------------------------------')
-    print('----------------------------------------')
     
 if __name__ == '__main__':
     main()
